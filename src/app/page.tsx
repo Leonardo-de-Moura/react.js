8ea00cad60bc8d476b5501d05a9b5744c52a4b3c
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-22 10:52:58 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+     <Button text="ls"/>
+     <Button text="hahaha"/>
+     <Button text="dd"/>
     </main>
   );
 }
